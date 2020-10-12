a57e92805bce160548cc921f88002a8051f1cd27
Indiorlei de Oliveira <indiorleioliveira@gmail.com>
2020-10-12 18:52:41 -0300

    add react-table exemple

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from 'react';
+import Table from './Table';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const doFetch = async () => {
+      const response = await fetch('https://randomuser.me/api/?results=100');
+      const body = await response.json();
+      const contacts = body.results;
+      setData(contacts);
+    };
+
+    doFetch();
+  });
+
+  const columnsTable = useMemo(
+    () => [
+      { Header: 'First Name', accessor: 'name.first' },
+      { Header: 'Last Name', accessor: 'name.last' },
+      { Header: 'Email', accessor: 'email' },
+      { Header: 'City', accessor: 'location.city' },
+    ],
+    [],
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Table columns={columnsTable} data={data} />
     </div>
   );
 }
